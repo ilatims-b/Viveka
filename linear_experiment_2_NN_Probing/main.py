79cd350ec674c0d5823464910786c6b021d2fe5a
Arsh Mathur <arshmathurj@gmail.com>
2025-08-04 17:20:47 +0530

    minor change

--- linear_experiment_2_NN_Probing/main.py
+++ linear_experiment_2_NN_Probing/main.py
@@ -1,4 +1,4 @@
-from utils import encode, generate, create_prompts, generate_model_answers, check_correctness, find_exact_answer_simple, exact_answer_direct, is_vague_or_non_answer, extract_answer_with_llm, _cleanup_extracted_answer, load_model, load_statements
+from utils import encode, generate, create_prompts, generate_model_answers, check_correctness, find_exact_answer_simple, extract_answer_direct, is_vague_or_non_answer, extract_answer_with_llm, _cleanup_extracted_answer, load_model, load_statements
 from hook import Hook, get_resid_acts
 import argparse
 from tqdm import tqdm
